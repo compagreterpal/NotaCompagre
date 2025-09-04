9f071ee80d158f26755e4f223263c41b381cc39e
Compagre Terpal <compagreterpal@gmail.com>
2025-09-04 09:56:27 +0700

    REVISI 5

--- NotaPerusahaan_Web/app.py
+++ NotaPerusahaan_Web/app.py
@@ -593,4 +593,5 @@ def export_data():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
